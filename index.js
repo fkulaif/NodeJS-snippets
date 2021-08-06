c2be52ba77f5b227ba2802c078e1f7a215fc2d8b
fkulaif <ferakulaif@gmail.com>
2021-08-06 14:51:26 -0400

    Parsing and Trimming Path Url

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-/**
- * Primary file for the API
+/*
+ * Primary file for API
+ *
  */
  
-//Dependencies
+// Dependencies
 var http = require('http');
+var url = require('url');
  
-//The server should respond to all requests with a string
-var server = http.createServer(function(req, res){
-    res.end('I am the server');
-});
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
  
-//Start the server, and have it listen on port 3000
-server.listen(3000, function(){
-    console.log('The server is listening on port 3000 now');
+  // Get the URL and parse it
+  var parsedUrl = url.parse(req.url, true);
+ 
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+ 
+  // Send the response
+  res.end('Request received on path: '+trimmedPath);
 });
+
+  // Start the server, and have it listen on port 3000
+  server.listen(3000, function(){
+  console.log('The server is listening on port 3000 now');
+  });
