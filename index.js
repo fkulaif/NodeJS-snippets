d64d16de5cadda117915e0041c4ff1adce97577d
fkulaif <ferakulaif@gmail.com>
2021-08-06 23:44:59 -0400

    adding url parse w/ parameters

--- index.js
+++ index.js
@@ -17,11 +17,17 @@ var server = http.createServer(function(req,res){
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
   // Get the HTTP method
   var method = req.method.toUpperCase();
 
   // Send the response
-  res.end('Request received on path: '+trimmedPath +' with Method: '+method);
+  res.end('Hello World');
+
+  // Log the request path
+  console.log('Request received on path: '+trimmedPath+' with Method: '+method+' query string parameters: ', queryStringObject);
 });
 
   // Start the server, and have it listen on port 3000
